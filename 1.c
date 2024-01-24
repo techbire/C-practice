76271ada8d45b7b092eb503d9be19128a489ce50
techbire <anshtechnical@gmail.com>
2024-01-24 21:22:49 +0530

    flaot to int

--- 1.c
+++ 1.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+// #include <stdio.h>
+// int main() {
+//    // printf() displays the string inside quotation
+//    printf("Hello, World!");
+//    return 0;
+// }
+
+#include<stdio.h>
+int  main(){
+     float x;
+     scanf("%f",&x);
+     int y;
+     y=(int)x;
+     printf("%d",y);
 }
 
