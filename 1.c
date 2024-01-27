1f968146ab2e36d4e2c95fa9e154047e8fe12807
techbire <anshtechnical@gmail.com>
2024-01-27 12:19:24 +0530

    changes

--- 1.c
+++ 1.c
@@ -161,15 +161,3 @@ int main() {
  return 0;
 }
 
-
-
-#include<stdio.h>
-
-int main() {
-    int a = 100;
-    for (int i = 1;a>0; i++) {
-        printf("%d ", a);
-        a = a - 3;
-    }
- return 0;
-}
