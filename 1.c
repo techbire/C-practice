01ff66e723fd6c57a5a8f4010bc8acd9d23163f1
Ansh Gupta <techbire69@gmail.com>
2024-01-28 17:46:30 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -163,3 +163,32 @@ int main() {
 
 
 
+//print the 100,97,93...0
+
+#include<stdio.h>
+
+int main() {
+    int a = 100;
+    for (int i = 1;a>0; i++) {
+        printf("%d ", a);
+        a = a - 3;
+    }
+ return 0;
+}
+
+
+
+
+//print the 100,97,93...0
+
+#include<stdio.h>
+
+int main() {
+    int a = 100;
+    for (int i = 1;a>0; i++) {
+        printf("%d ", a);
+        a = a - 3;
+    }
+ return 0;
+}
+
