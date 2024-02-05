d97b0263fafd39aa08ae757095fbfad6a781f31c
techbire <anshtechnical@gmail.com>
2024-02-05 22:30:18 +0530

    added

--- 1.c
+++ 1.c
@@ -150,45 +150,32 @@
 
 //print the 100,97,93...0
 
+// #include<stdio.h>
+
+// int main() {
+//     int a = 100;
+//     for (int i = 1;a>0; i++) {
+//         printf("%d ", a);
+//         a = a - 3;
+//     }
+//  return 0;
+// }
+
+
+
+// prime checker
 #include<stdio.h>
-
-int main() {
-    int a = 100;
-    for (int i = 1;a>0; i++) {
-        printf("%d ", a);
-        a = a - 3;
-    }
- return 0;
-}
-
-
-
-//print the 100,97,93...0
-
-#include<stdio.h>
-
-int main() {
-    int a = 100;
-    for (int i = 1;a>0; i++) {
-        printf("%d ", a);
-        a = a - 3;
-    }
- return 0;
-}
-
-
-
-
-//print the 100,97,93...0
-
-#include<stdio.h>
-
-int main() {
-    int a = 100;
-    for (int i = 1;a>0; i++) {
-        printf("%d ", a);
-        a = a - 3;
-    }
- return 0;
-}
-
+int main(){
+int n;
+printf("enter a number");
+scanf("%d",&n);
+int a=0;
+for(int i=2;i<n-1;i++){
+if(n%i==0){
+a=1;
+break;
+}}
+if (n==1) printf("1 is neither prime nor composite");
+else if (a==0) printf("the given number is prime");
+else printf("the given number is composite");
+return 0;}
