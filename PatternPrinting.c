e6c8b76fa519a07ab3ab24e2df22d02309112b20
techbire <anshtechnical@gmail.com>
2024-02-12 23:11:51 +0530

    added

---
+++ PatternPrinting.c
@@ -0,0 +1,38 @@
+// printing squared stars with input based
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+// printing stars with input based r and c
+// #include <stdio.h>
+// int main()
+// {
+//     int r, c;
+//     printf("Enter row: ");
+//     scanf("%d", &r);
+//     printf("Enter column: ");
+//     scanf("%d", &c);
+//     for (int i = 1; i <= r; i++)
+//     {
+//         for (int i = 1; i <= c; i++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
