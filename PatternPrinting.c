11931a491a43b9b33dea274a665244ebc2947256
techbire <anshtechnical@gmail.com>
2024-02-13 22:31:19 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -37,55 +37,51 @@
 //     }
 // }
 
+// printing star trinagle
+//  #include<stdio.h>
+//  int main(){
+//  int n;
+//  printf("enter a number: ");
+//  scanf("%d",&n);
+//  for(int i=1;i<=n;i++){
+//      for(int j=1;j<=i;j++){
+//          printf("*");
+//      }
+//  printf("\n");
 
-//printing star trinagle
-// #include<stdio.h>
-// int main(){
-// int n;
-// printf("enter a number: ");
-// scanf("%d",&n);
-// for(int i=1;i<=n;i++){
-//     for(int j=1;j<=i;j++){
-//         printf("*");
-//     }
+// }
+
+// }
+
+// printing reverse star triangle
+// method-1
+//  #include<stdio.h>
+//  int main(){
+//  int n;
+//  printf("enter a number: ");
+//  scanf("%d",&n);
+//  for(int i=1;i<=n;i++){
+//      for(int j=1;j<=n+1-i;j++){
+//          printf("*");
+//      }
+
 // printf("\n");
 
 // }
 
 // }
 
-
-
-//printing reverse star triangle
-//method-1
-// #include<stdio.h>
-// int main(){
-// int n;
-// printf("enter a number: ");
-// scanf("%d",&n);
-// for(int i=1;i<=n;i++){
-//     for(int j=1;j<=n+1-i;j++){
-//         printf("*");
-//     }
-
-// printf("\n");
-
-// }
-
-// }
-
-
-//printing reverse star triangle
-//method-2
-// #include<stdio.h>
-// int main(){
-// int n;
-// printf("enter a number: ");
-// scanf("%d",&n);
-// int a=n;
-// for(int i=1;i<=n;i++){
-//     for(int j=1;j<=a;j++){
-//         printf("*");
+// printing reverse star triangle
+// method-2
+//  #include<stdio.h>
+//  int main(){
+//  int n;
+//  printf("enter a number: ");
+//  scanf("%d",&n);
+//  int a=n;
+//  for(int i=1;i<=n;i++){
+//      for(int j=1;j<=a;j++){
+//          printf("*");
 
 //     }
 // a--;
@@ -95,24 +91,20 @@
 
 // }
 
-
-
-
-//printing number triangle
-// #include<stdio.h>
-// int main(){
-// int n;
-// printf("enter a number: ");
-// scanf("%d",&n);
-// for(int i=1;i<=n;i++){
-//     for(int j=1;j<=i;j++){
-//     printf("%d",j);
-// }
-// printf("\n");
-// }
+// printing number triangle
+//  #include<stdio.h>
+//  int main(){
+//  int n;
+//  printf("enter a number: ");
+//  scanf("%d",&n);
+//  for(int i=1;i<=n;i++){
+//      for(int j=1;j<=i;j++){
+//      printf("%d",j);
+//  }
+//  printf("\n");
+//  }
 
 // }
-
 
 //printing odd number triangle
 #include<stdio.h>
