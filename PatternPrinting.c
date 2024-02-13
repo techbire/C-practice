b1dfa85c87840e5ef68bfbf44cc5c4db85baa4e6
techbire <anshtechnical@gmail.com>
2024-02-13 22:23:56 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -99,16 +99,33 @@
 
 
 //printing number triangle
+// #include<stdio.h>
+// int main(){
+// int n;
+// printf("enter a number: ");
+// scanf("%d",&n);
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//     printf("%d",j);
+// }
+// printf("\n");
+// }
+
+// }
+
+
+//printing odd number triangle
 #include<stdio.h>
 int main(){
 int n;
 printf("enter a number: ");
 scanf("%d",&n);
 for(int i=1;i<=n;i++){
-    for(int j=1;j<=i;j++){
-    printf("%d",j);
+    int a=1;
+    for(int j=1;j<=i;j++ ){
+        printf("%d",a);
+        a=a+2;
+    }
+    printf("\n");
 }
-printf("\n");
-}
-
 }
