fa54feb983f843408aec33f3b582c6afb49259de
techbire <anshtechnical@gmail.com>
2024-02-13 21:46:18 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -55,4 +55,42 @@
 // }
 
 
+
 //printing reverse star triangle
+//method-1
+// #include<stdio.h>
+// int main(){
+// int n;
+// printf("enter a number: ");
+// scanf("%d",&n);
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n+1-i;j++){
+//         printf("*");
+//     }
+
+// printf("\n");
+
+// }
+
+// }
+
+
+//printing reverse star triangle
+//method-2
+#include<stdio.h>
+int main(){
+int n;
+printf("enter a number: ");
+scanf("%d",&n);
+int a=n;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=a;j++){
+        printf("*");
+
+    }
+a--;
+printf("\n");
+
+}
+
+}
