25597c5ae67e48a1cb4b85968642d64c17fd7e86
techbire <anshtechnical@gmail.com>
2024-02-15 22:00:40 +0530

    added

--- Loops.c
+++ Loops.c
@@ -268,3 +268,37 @@
 // printf("result:%d ",result);
 
 // }
+
+//checking armstrong number
+#include<stdio.h>
+int main(){
+    int number,count=0,rem,result=0,mul=1,cnt;   
+    printf("enter a number: ");
+    scanf("%d",&number);
+    int q=number;
+    while(q!=0)
+    {
+        q=q/10;
+        count++;
+    }
+    cnt=count;
+    q=number;
+    while(q!=0)
+    {
+        rem=q%10;
+        while(cnt!=0)
+        {
+            mul=mul*rem;
+            cnt--;
+        }
+        result=result+mul;
+        cnt=count;
+        q=q/10;
+        mul=1;
+    }
+    if(result==number)
+    printf("%d is an Armstrong nummber",number);
+    else
+    printf("%d is not an Armstrong number",number);
+    return 0;
+}
