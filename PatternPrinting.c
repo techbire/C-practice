a5fccf2b00eedf1cd6a2becc7624638e9d1f1064
techbire <anshtechnical@gmail.com>
2024-02-22 03:14:17 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -175,3 +175,21 @@
 //     return 0;
 // }
 
+
+//print starplus
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for (int j=1;j<=n;j++){
+//             int a=n/2+1;
+//             if (j==a||i==a){
+    
+//             printf("*");}
+//             else{printf(" ");}
+//         }
+//         printf("\n");
+//     }
+// }
