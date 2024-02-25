1f429b00640f4c3e11f4b528356a453a5907064e
Ansh Gupta <techbire69@gmail.com>
2024-02-25 23:56:45 +0530

    Update PatternPrinting.c

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -239,3 +239,25 @@
 //     }
 // return 0;
 // }
+
+
+//floyd's triangle
+#include <stdio.h>
+
+int main() {
+    int rows, num = 1;
+
+    printf("Enter the number of rows for Floyd's Triangle: ");
+    scanf("%d", &rows);
+
+    printf("Floyd's Triangle:\n");
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
