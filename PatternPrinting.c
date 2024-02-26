0893746fb3e8b3c5ceef028cd38ec833e799cc77
techbire <anshtechnical@gmail.com>
2024-02-27 01:54:29 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -196,7 +196,7 @@
 
 
 
-//printing hollow triangular star
+// printing hollow triangular star
 // #include<stdio.h>
 // int main() {
 //     int r, c;
@@ -242,22 +242,16 @@
 
 
 //floyd's triangle
-// #include <stdio.h>
-
-// int main() {
-//     int rows, num = 1;
-
-//     printf("Enter the number of rows for Floyd's Triangle: ");
-//     scanf("%d", &rows);
-
-//     printf("Floyd's Triangle:\n");
-//     for (int i = 1; i <= rows; i++) {
-//         for (int j = 1; j <= i; j++) {
-//             printf("%d ", num);
-//             num++;
+// #include<stdio.h>
+// int main(){
+// int n,a=1;
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("%d",a);
+//            a++;  
 //         }
+       
 //         printf("\n");
-//     }
-
-//     return 0;
-// }
+//         }
+//     }    
