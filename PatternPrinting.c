282fc68616f752dc430c06afad6f7e42e39c3ba6
techbire <anshtechnical@gmail.com>
2024-02-27 02:13:03 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -242,16 +242,16 @@
 
 
 //floyd's triangle
-// #include<stdio.h>
-// int main(){
-// int n,a=1;
-//     scanf("%d",&n);
-//     for(int i=1;i<=n;i++){
-//         for(int j=1;j<=i;j++){
-//             printf("%d",a);
-//            a++;  
-//         }
+#include<stdio.h>
+int main(){
+int n,a=1;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",a);
+           a++;  
+        }
        
-//         printf("\n");
-//         }
-//     }    
+        printf("\n");
+        }
+    }    
