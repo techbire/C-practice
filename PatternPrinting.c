b5f66a759de0ab2b1a64bd488425058e5c2d95a4
techbire <anshtechnical@gmail.com>
2024-02-28 00:33:56 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -293,3 +293,66 @@
 //         printf("\n");
 //     }
 // }
+
+
+//printing star triangle mast
+// #include<stdio.h>
+// int main(){
+//     int n;
+// scanf("%d",&n);
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n-i;j++){
+//         printf(" "); }
+//         for(int k=1;k<=i;k++)
+//         {printf("*");}
+
+// printf("\n");
+// }
+//}
+
+
+
+//printing rhombus
+
+// #include <stdio.h>
+
+// int main() {
+// int n;
+// scanf("%d",&n);
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=((n*2)-i);j++){
+//        if(j<=n-i){printf(" ");}
+//        else{printf("*");}
+//     }
+//     printf("\n");
+// }
+
+//     return 0;
+// }
+
+//method-02
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++) {
+//         // Print spaces before the first 'x' in each row
+//         for (int j = 1; j <= n - i; j++) {
+//             printf(" ");
+//         }
+
+//         // Print 'x' characters
+//         for (int j = 1; j <= n; j++) {
+//             printf("x");
+//         }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+
+//printing
