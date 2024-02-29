8c6c4837a30530ae15bc481f4fb8af5e997b6e5a
techbire <anshtechnical@gmail.com>
2024-02-29 20:12:05 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -402,3 +402,30 @@
 
 //     return 0;
 // }
+
+
+
+//StarPyramid
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     printf("Enter the number of rows for the star pyramid: ");
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++) {
+//         // Print spaces
+//         for (int j = 1; j <= n - i; j++) {
+//             printf(" ");
+//         }
+
+//         // Print stars
+//         for (int k = 1; k <= 2 * i - 1; k++) {
+//             printf("*");
+//         }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
