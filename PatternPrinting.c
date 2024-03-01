7de04cefb9df6dc1b92d2aa37762d2794df05899
techbire <anshtechnical@gmail.com>
2024-03-01 22:00:06 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -429,3 +429,30 @@
 
 //     return 0;
 // }
+
+
+//NumberPyramid
+
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     printf("Enter the number of rows for the number pyramid: ");
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++) {
+//         // Print spaces
+//         for (int j = 1; j <= n - i; j++) {
+//             printf(" ");
+//         }
+
+//         // Print increasing numbers
+//         for (int k = 1; k <= i; k++) {
+//             printf("%d ", k);
+//         }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
