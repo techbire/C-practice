cb23a529e6d2447cc469db47c3bc49efe5adb801
Ansh Gupta <techbire69@gmail.com>
2024-03-17 23:34:29 +0530

    Update Function.c

--- Function.c
+++ Function.c
@@ -224,3 +224,36 @@
 //     return 0;
 // }
 
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int binomialCoefficient(int n, int k) {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+void printPascalsTriangle(int numRows) {
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", binomialCoefficient(i, j));
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int numRows;
+
+    printf("Enter the number of rows for Pascal's Triangle: ");
+    scanf("%d", &numRows);
+
+    printPascalsTriangle(numRows);
+
+    return 0;
+}
