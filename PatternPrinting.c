89d3d9cc45f3437ee060afe716541f796aa69822
techbire <anshtechnical@gmail.com>
2024-03-21 03:21:21 +0530

    added

--- PatternPrinting.c
+++ PatternPrinting.c
@@ -486,38 +486,38 @@
 
 
 // Star Diamond
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
 
-    int nsp = n / 2;
-    int nst = 1;
-    int ml = ((n / 2) + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= nsp; j++)
-        {
-            printf(" ");
-        }
-        for (int k = 1; k <= nst; k++)
-        {
-            printf("*");
-        }
+//     int nsp = n / 2;
+//     int nst = 1;
+//     int ml = ((n / 2) + 1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= nsp; j++)
+//         {
+//             printf(" ");
+//         }
+//         for (int k = 1; k <= nst; k++)
+//         {
+//             printf("*");
+//         }
 
-        if (i < ml)
-        {
-            nsp--;
-            nst += 2;
-        }
-        else
-        {
-            nsp++;
-            nst -= 2;
-        }
+//         if (i < ml)
+//         {
+//             nsp--;
+//             nst += 2;
+//         }
+//         else
+//         {
+//             nsp++;
+//             nst -= 2;
+//         }
 
-        printf("\n");
-    }
-}
+//         printf("\n");
+//     }
+// }
