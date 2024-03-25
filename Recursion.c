aff5864ce32f6410cf851ba1100af4bcb7bbdd68
Ansh Gupta <anshtechnical@gmail.com>
2024-03-26 00:04:54 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -15,28 +15,59 @@
 
 
 //Print ‘n’ to ‘1’
-#include <stdio.h>
+// #include <stdio.h>
 
 
-void printNumbers(int n) {
+// void printNumbers(int n) {
 
-    if (n == 0)
-        return;
+//     if (n == 0)
+//         return;
     
 
-    printf("%d\n", n);
+//     printf("%d\n", n);
 
-    printNumbers(n - 1);
-}
+//     printNumbers(n - 1);
+// }
 
-int main() {
-    int n;
+// int main() {
+//     int n;
 
-    printf("Enter a number: ");
-    scanf("%d", &n);
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
 
-    printf("Numbers from %d to 1:\n", n);
-    printNumbers(n);
+//     printf("Numbers from %d to 1:\n", n);
+//     printNumbers(n);
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+// Print ‘1’ to ‘n’ (Extra Parameter)
+// #include <stdio.h>
+
+
+// void printNumbers(int n) {
+
+//     if (n == 1) {
+//         printf("1\n");
+//         return;
+//     }
+    
+
+//     printNumbers(n - 1);
+    
+
+//     printf("%d\n", n);
+// }
+
+// int main() {
+//     int n;
+
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
+
+//     printf("Numbers from 1 to %d:\n", n);
+//     printNumbers(n);
+
+//     return 0;
+// }
