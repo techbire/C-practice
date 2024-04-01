2cfee0424cfe2c3c1d03a83e1e0edacbd26eab6d
techbire <anshtechnical@gmail.com>
2024-04-02 00:06:46 +0530

    added

--- arrays.c
+++ arrays.c
@@ -13,3 +13,14 @@
 //      printf("%d ",arr[i]); 
 // }}
 
+
+//Prob 1 : Given array of marks, print marks less than 35
+// #include<stdio.h>
+// int main(){
+//     int marks[10]={34,37,65,27,23,69,97,56,45,8};
+//     for(int i=0;i<10;i++){
+//         if (marks[i]<35){
+//         printf("%d ",i);}
+//     }
+// return 0;
+// }
