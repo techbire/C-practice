0547ea8b77c98d388c4b19115c05760eb196999f
Ansh Gupta <anshtechnical@gmail.com>
2024-04-03 02:09:00 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -52,4 +52,37 @@
 // }
 
 
-//
+//finding maximum element
+// #include <stdio.h>
+
+// int main() {
+//    int arr[5]={3,4,5,2,5}; 
+//    int max=arr[0];
+//    for(int i=1; i<5; i++){ 
+//        if(max<arr[i]){
+//            max=arr[i];
+//        } 
+//    }
+//    printf("%d",max);
+//    return 0;
+// }
+
+
+//swapping number with arrays
+
+// #include <stdio.h>
+
+// void fun(int x[]){
+//     int temp=x[0];
+//     x[0]=x[1];
+//     x[1]=temp;
+//     return;
+// }
+// int main(){
+//     int arr[]={2,3};
+//     fun(arr);
+//     printf("after swapping:%d,%d",arr[0],arr[1]);
+//     return 0;
+// }
+
+
