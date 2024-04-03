bf590c0da114bebf9c0a4691024353f4edeb9b0f
Ansh Gupta <anshtechnical@gmail.com>
2024-04-03 10:55:02 +0530

    Update arrays2D.c

--- arrays2D.c
+++ arrays2D.c
@@ -29,3 +29,33 @@
 //     return 0;
 // }
 
+
+//sum of given matrix
+// #include <stdio.h>
+
+// int main() {
+//     int sum, m, n, i, j;
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &m);
+//     printf("Enter the number of columns: ");
+//     scanf("%d", &n);
+
+   
+//     int a[m][n];
+
+//     printf("Enter the elements of the array:\n");
+//     for (i = 0; i < m; i++) {
+//         for (j = 0; j < n; j++) {
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     printf("The sum of array elements are:\n");
+//     for (i = 0; i < m; i++) {
+//         for (j = 0; j < n; j++) {
+//             sum+=a[i][j];
+//         }
+//     }
+//  printf("%d ", sum); 
+//     return 0;
+// }
