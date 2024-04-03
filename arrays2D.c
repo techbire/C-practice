8a6c3178d1e250f735e463868a1f3e61187f3c13
Ansh Gupta <anshtechnical@gmail.com>
2024-04-03 10:48:03 +0530

    Update arrays2D.c

--- arrays2D.c
+++ arrays2D.c
@@ -1,1 +1,31 @@
+//take input and print output
+// #include <stdio.h>
+
+// int main() {
+//     int m, n, i, j;
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &m);
+//     printf("Enter the number of columns: ");
+//     scanf("%d", &n);
+
+   
+//     int a[m][n];
+
+//     printf("Enter the elements of the array:\n");
+//     for (i = 0; i < m; i++) {
+//         for (j = 0; j < n; j++) {
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     printf("The array elements are:\n");
+//     for (i = 0; i < m; i++) {
+//         for (j = 0; j < n; j++) {
+//             printf("%d ", a[i][j]); 
+//         }
+//         printf("\n");  
+//     }
+
+//     return 0;
+// }
 
