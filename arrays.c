01fa841813e1dcab4622895c6a968edf6fd98983
Ansh Gupta <anshtechnical@gmail.com>
2024-04-04 21:49:42 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -148,3 +148,26 @@
 
 //     return 0;
 // }
+
+//second highest number
+
+// #include <stdio.h>
+// #include<limits.h>
+
+// int main() {
+// int arr[7]={1,2,3,4,4,2,1};
+// int max=INT_MIN;
+// int smax=INT_MIN;
+// for(int i=0;i<=6;i++){
+//     if(max<arr[i]){
+//         smax=max;
+//         max=arr[i];
+//     }
+//     else if(smax<arr[i]){
+//         smax=arr[i];
+//     }
+// }
+// printf("%d",smax);
+// }
+
+
