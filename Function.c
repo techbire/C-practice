8bb7684203c8a174355c929253374cb79d89d9be
Ansh Gupta <anshtechnical@gmail.com>
2024-04-06 22:44:32 +0530

    Update Function.c

--- Function.c
+++ Function.c
@@ -404,8 +404,8 @@
 //         printf("%d is not an Armstrong number.\n", original);
 // }
 
-if sum!=original
-    while (n > 0) {
+// if sum!=original
+//     while (n > 0) {
 //         int digit = n % 10;
 //         sum += digit * digit * digit;
 //         n /= 10;
