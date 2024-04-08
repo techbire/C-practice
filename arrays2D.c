3ccdeac203356bc73b14c8589e983555a2e564a0
Ansh Gupta <anshtechnical@gmail.com>
2024-04-08 21:55:59 +0530

    Update arrays2D.c

--- arrays2D.c
+++ arrays2D.c
@@ -118,6 +118,26 @@
 //     return 0;
 // }
 
+//calculate lower triangular matrix
+// #include <stdio.h>
+
+// int main() {
+//     int size, sum = 0;
+//     scanf("%d", &size);
+//     int arr[size][size];
+//     for (int i = 0; i < size; i++) {
+//         for (int j = 0; j < size; j++) {
+//             scanf("%d", &arr[i][j]);
+//         }
+//     }
+//     for (int i = 1; i < size; i++) {
+//         for (int j = 0; j < i; j++) {
+//             sum += arr[i][j];
+//         }
+//     }
+//     printf("%d", sum);  
+//     return 0;
+// }
 
 
 
