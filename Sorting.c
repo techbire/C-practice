1358a729fd4072f87e3e1a688a59190c849c501b
Ansh Gupta <anshtechnical@gmail.com>
2024-04-14 13:12:11 +0530

    Update Sorting.c

--- Sorting.c
+++ Sorting.c
@@ -35,3 +35,36 @@
 
 //------------------------------------------------------
 //selection sorting:
+// #include<stdio.h>
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+
+//     // Input array elements
+//     for(int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     // Selection sort
+//     for(int i = 0; i < n - 1; i++) {
+//         int smallest = i;
+//         for(int j = i + 1; j < n; j++) {
+//             if(arr[j] < arr[smallest]) {
+//                 smallest = j;
+//             }
+//         }
+//         // Swap arr[smallest] and arr[i]
+//         int temp = arr[smallest];
+//         arr[smallest] = arr[i];
+//         arr[i] = temp;
+//     }
+
+//     // Output sorted array
+//     for(int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+
+//     return 0;
+// }
