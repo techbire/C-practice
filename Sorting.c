af7bf1e0c7219824c080a7301e606c8e25372152
Ansh Gupta <anshtechnical@gmail.com>
2024-04-14 13:51:43 +0530

    Update Sorting.c

--- Sorting.c
+++ Sorting.c
@@ -68,3 +68,42 @@
 
 //     return 0;
 // }
+
+
+//------------------------------------------------------
+//insertion sorting:
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     printf("Enter the number of elements: ");
+//     scanf("%d", &n);
+
+//     int arr[n];
+
+//     // Input array elements
+//     printf("Enter %d elements:\n", n);
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     // Insertion sort
+//     for (int i = 1; i < n; i++) {
+//         int key = arr[i];
+//         int j = i - 1;
+//         while (j >= 0 && arr[j] > key) {
+//             arr[j + 1] = arr[j];
+//             j--;
+//         }
+//         arr[j + 1] = key;
+//     }
+
+//     // Output sorted array
+//     printf("Sorted array:\n");
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
