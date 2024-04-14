21caa97c277f26a1bd85a2643e6de55e7ff19b47
Ansh Gupta <anshtechnical@gmail.com>
2024-04-14 13:08:41 +0530

    Create Sorting.c

---
+++ Sorting.c
@@ -0,0 +1,37 @@
+//------------------------------------------------------
+//bubble sorting:
+// #include<stdio.h>
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+
+//     // Input array elements
+//     for(int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     // Bubble sort
+//     for(int i = 0; i < n - 1; i++) {
+//         for(int j = 0; j < n - 1 - i; j++) {
+//             if(arr[j] > arr[j + 1]) {
+//                 // Swap
+//                 int temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+
+//     // Output sorted array
+//     for(int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+
+//     return 0;
+// }
+
+
+//------------------------------------------------------
+//selection sorting:
