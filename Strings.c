0b48a703334e362c0a90f3d4ec2f9c98a9552000
Ansh Gupta <anshtechnical@gmail.com>
2024-05-02 00:13:22 +0530

    Update and rename Strings to Strings.c

---
+++ Strings.c
@@ -0,0 +1,28 @@
+// reversing the string
+
+// #include <stdio.h>
+// #include<string.h>
+
+
+// int main()
+// {
+// char str[100];
+// puts("enter the sentence");
+// scanf("%[^\n]s",str);
+
+// int size,k;
+// while(str[k]!='\0'){
+//     size++;
+//     k++;
+// }
+
+// for(int i=0,j=size-1;i<=j;i++,j--){
+//     char temp=str[i];
+//     str[i]=str[j];
+//     str[j]=temp;
+// }
+// puts("reverse sentence is: ");
+// puts(str);
+
+
+// }
